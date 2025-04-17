046cd51170c56366a548fccc0ab7bbde5442a31f
SIMONE MUSSINI <284900@studenti.unimore.it>
2025-04-17 17:02:53 +0200

    Esame dovrebbe essere fatto

--- Esame.c
+++ Esame.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define NUM_PEDONI 10
+
 struct passerella_t {
 	sem_t mutex;
 
@@ -9,23 +18,67 @@ struct passerella_t {
 	sem_t s_pedone;
 } passerella;
 
-void init_passerella() {
-	sem_init(&passerella->mutex, 0, 1);
-	sem_init(&passerella->s_guardiano, 0, 0); // Devo inizializzare il semaforo a 0 invece che 1 
-	sem_init(&passerella->s_pedone, 0, 0);
-	passerella->pedoni_attivi = passerella->pedoni_bloccati = 0;
-	passerella->barca_arrivata = 1;		// Perchè parto con il ponte aperto
+
+/*
+	EDIT: Codice originale: 
+	void init_passerella(struct passerella_t *passerella) {
+		...
+		sem_init(&passerella->s_guardiano, 0, 1);
+		...
+	}
+
+	Nella versione originale inizializzo erroneamente il semaforo del guardiano a 1. Questo aumenta il valore della post del semaforo s_guardiano di 1 rendendo quindi inutili tutte le wait successive.
+	Per risolvere basta semplicemente inizializzare il valore a 0.
+
+*/
+void init_passerella(struct passerella_t *passerella) {
+	sem_init(&passerella->mutex, 0, 1);				// Semaforo che si comporta da mutex
+	sem_init(&passerella->s_guardiano, 0, 0);	// Semaforo che gestisce il guardiano
+	sem_init(&passerella->s_pedone, 0, 0);		// Semaforo che gestisce i pedoni
+	passerella->pedoni_attivi = passerella->pedoni_bloccati = 0;		// Variabili counter per gestire i pedoni che stanno attraversati sul ponte e quelli che stannoa aspettando
+	passerella->barca_arrivata = 1;		// Varibile per gestire lo stato del ponte (barca_arrivata = 1 -> ponte alzato, barca_arrivata = 0 -> ponte abbassato)
 }
 
+
+/*
+	EDIT: Codice originale: 
+	int pedone_entro_passerella(struct passerella_t *passerella, int hofretta) {
+		sem_wait(&passerella->mutex);
+
+		int ritorno = 1;
+
+		if(passerella->barca_arrivata == 0) {
+			passerella->pedoni_attivi++;
+			sem_post(&passerella->s_pedone);
+		} else {
+			passerella->pedoni_bloccati++;
+			if (hofretta) ritorno = 0;
+		}
+		
+		sem_post(&passerella->mutex);
+		sem_wait(&passerella->s_pedone);
+
+		return ritorno;
+	}
+
+		
+	Con la versione originale faccio la wait del semaforo anche quando il valore di ritorno è 0, quindi quando il pedone ha fretta e dovrebbe invece scegliere un altra strada. 
+	Per risolvere ciò devo ritornare direttamente 0, ricordandomi però di rilasciare prima il mutex.
+
+*/
 int pedone_entro_passerella(struct passerella_t *passerella, int hofretta) {
 	sem_wait(&passerella->mutex);
 
-	if (passerella->barca_arrivata == 0) {
-		passerella->pedoni_attivi++;
+	if (passerella->barca_arrivata == 0) {		// Se il ponte è abbassato faccio preventive post
+		passerella->pedoni_attivi++;			// Sto attraversando il ponte
 		sem_post(&passerella->s_pedone);
 	} else {
-		passerella->pedoni_bloccati++;
-		if (hofretta) return 0;	// Devo fare un return altrimenti mi blocco comunque
+		
+		if (hofretta) {				// Se ho fretta e il ponte è alzato me ne vado
+			sem_post(&passerella->mutex);
+			return 0;	
+		}
+		passerella->pedoni_bloccati++;		// Se non ho fretta mi blocco alla wait di seguito e aumento il counter. Posso farlo di seguito all'if perché tanto se ho fretta sono già uscito dalla funzione
 	}
 
 	sem_post(&passerella->mutex);
@@ -36,7 +89,7 @@ int pedone_entro_passerella(struct passerella_t *passerella, int hofretta) {
 void pedone_esco_passerella(struct passerella_t *passerella) {
 	sem_wait(&passerella->mutex);
 
-	passerella->pedoni_attivi--;
+	passerella->pedoni_attivi--;		// Un pedone è uscito dal ponte
 
 	// Se sono l'ultimo a passare sveglio il guardiano
 	if (passerella->pedoni_attivi == 0 && passerella->barca_arrivata) {
@@ -44,15 +97,14 @@ void pedone_esco_passerella(struct passerella_t *passerella) {
 	}
 
 	sem_post(&passerella->mutex);
-	return 1;
 }
 
 void guardiano_abbassa_passerella(struct passerella_t *passerella) {
 	sem_wait(&passerella->mutex);
 
-	passerella->barca_arrivata = 0;
+	passerella->barca_arrivata = 0;	// Il ponte è abbassato
 
-	while(passerella->pedoni_bloccati) {
+	while(passerella->pedoni_bloccati) {		// Sveglio tutti i pedoni
 		passerella->pedoni_bloccati--;
 		passerella->pedoni_attivi++;
 		sem_post(&passerella->s_pedone);
@@ -64,26 +116,37 @@ void guardiano_abbassa_passerella(struct passerella_t *passerella) {
 void guardiano_alza_passerella(struct passerella_t *passerella) {
 	sem_wait(&passerella->mutex);
 
-	passerella->barca_arrivata = 1;
+	passerella->barca_arrivata = 1;	// Il ponte è stato alzato
 
-	if(passerella->pedoni_attivi == 0) {
+	if(passerella->pedoni_attivi == 0) {		// Se non ci sono pedoni sul ponte faccio preventive post
 		sem_post(&passerella->s_guardiano);
 	}
 
 	sem_post(&passerella->mutex);
-	sem_wait(&passerella->s_guardiano);
+	sem_wait(&passerella->s_guardiano);		// Se c'erano dei pedoni sul ponte mi blocco
 }
 
+void pausetta(int quanto) {
+  struct timespec t;
+  t.tv_sec = 0;
+  t.tv_nsec = (rand()%100+1)*1000000 + quanto;
+  nanosleep(&t,NULL);
+}
 
 void *pedone(void *arg) {
 	while(1) {
-		printf("Sono arrivato alla passerella!\n");
-		if(pedone_entro_passerella(&passerella, 1)) {
-			printf("Sto attraversando la passerella!\n");
+		pausetta(1000000);
+		int priorità = rand()%2;
+		printf("%lu - Sono arrivato alla passerella con priorità %d!\n", pthread_self(), priorità);
+		if(pedone_entro_passerella(&passerella, priorità)) {
+			printf("%lu - Sto attraversando la passerella!\n", pthread_self());
+
+
+			pausetta(1000000);
 			pedone_esco_passerella(&passerella);
-			printf("Sono uscito dalla passerella!\n");
+			printf("%lu - Sono uscito dalla passerella!\n", pthread_self());
 		} else {
-			printf("Faccio un altra strada!\n");
+			printf("%lu - Faccio un altra strada!\n", pthread_self());
 		}
 	}
 	return 0;
@@ -92,10 +155,44 @@ void *pedone(void *arg) {
 
 void *guardiano(void *arg) {
 	while(1) {
+		pausetta(1000000);
 		guardiano_abbassa_passerella(&passerella);
-		printf("Attendi la barca!\n");
+		printf("Abbasso la passerella!\n");
+		
+		printf("Aspetto che arrivi una barca...\n");
 
+		pausetta(1000000);
+		printf("Barca arrivata!\n");
 		guardiano_alza_passerella(&passerella);
 		printf("La barca è passata!\n");
 	}
 }
+
+/* la creazione dei thread */
+
+int main() {
+  pthread_attr_t a;
+  pthread_t p;
+
+  
+  /* inizializzo il mio sistema */
+  init_passerella(&passerella);
+
+  srand(time(NULL));
+
+  pthread_attr_init(&a);
+  pthread_attr_setdetachstate(&a, PTHREAD_CREATE_DETACHED);
+
+  for (int i=0; i<NUM_PEDONI; i++)
+    pthread_create(&p, &a, pedone, (void *)(i));
+  
+  pthread_create(&p, &a, guardiano, NULL);
+
+  pthread_attr_destroy(&a);
+
+  sleep(1);
+
+  return 0;
+}
+
+
